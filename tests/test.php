e07f2fffe5e0917d3bb12177d4c6d3bdf57133b0
Gabriel Guerreiro <gaguerreiro@gmail.com>
2015-05-07 00:15:12 -0300

    File Test

--- tests/test.php
+++ tests/test.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once './vendor/autoload.php';
 
 $options = array(
 	'channel' => '#general',
 	'username' => 'Robot'
 	);
 
-$ss = new SimpleSlack\Client("https://hooks.slack.com/services/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", $options);
+$ss = new SimpleSlack\Client("[YOUR TEAM ENDPOINT]", $options);
 $ss->sendMessage('Message test');
 
 ?>
