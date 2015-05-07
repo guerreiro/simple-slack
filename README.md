73cdaba9c6f11f8cddb93d54f3a3d2984355297e
Gabriel Guerreiro <gaguerreiro@gmail.com>
2015-05-06 23:21:03 -0300

    Composer change

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # simple-slack
 PHP class to help with Slack Incoming WebHooks
+
+## Requirements
+
+* PHP 5.3 or greater
+
+## Installation
+
+You can install the package using the [Composer](https://getcomposer.org/) package manager. You can install it by running this command in your project root:
+
+```sh
+composer require gaguerreiro/simple-slack
--- tests/test.php
+++ tests/test.php
@@ -2,14 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use SimpleSlack\Client;
-
 $options = array(
 	'channel' => '#general',
 	'username' => 'Robot'
 	);
 
-$ss = new Client("https://hooks.slack.com/services/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", $options);
+$ss = new SimpleSlack\Client("https://hooks.slack.com/services/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", $options);
 $ss->sendMessage('Message test');
 
 ?>
